d3c17e278dabf6824b1b4ff87a691b7b172bdc34
Jonathan Ellis-Craig <jonathan.craig@wipro.com>
2017-01-17 11:32:32 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+# Blabbr
+
+A simple API to store and retrieve comments from a Firebase database.
+ 
+## Getting started
+
+- Clone this repo and 'npm install' dependencies.
+- Rename the config.default.json to config.json
+- Enter your API details from Firebase into the config.json file
+- Type 'npm start'
+
+The project will fire up a server hosted on port 3000.
+
+## Adding a comment
+
+Create a 'POST' request to http://localhost:3000/[COMPONENTID] with the following JSON as you body:
+
+```JSON
+{
+    userName: STRING,
+    userEmail: STRING,
+    comment: STRING
+}
+```
+
+e.g.
+```
+{
+	"userName": "George",
+	"userEmail": "george.smith@wipro.com",
+	"comment": "Looks good - approved!"
+}
+```
+
+If the POST was successfull you'll recieve the following JSON back:
+```
+{
+  "success": 1  
+}
+```
+
+## Retrieving a comment
+
+Create a 'GET' request to http://localhost:3000/[COMPONENTID]
+
+Here's a sample of the data that would be returned
+
+```
+{
+  "success": 1,
+  "comments": {
+    "undefined": {
+      "-Kag04f2kgW1a0NLErbJ": {
+        "comment": "Please add a 2px black border",
+        "componentId": "/dropdown",
+        "timestamp": 1484649683645,
+        "userEmail": "tim.sail@wipro.com",
+        "userName": "Tim"
+      },
+      "-Kag0Kt2cjeA-7YyHrrR": {
+        "comment": "Looks good - approved!",
+        "componentId": "/dropdown",
+        "timestamp": 1484649752200,
+        "userEmail": "george.smith@wipro.com",
+        "userName": "George"
+      }
+    }
+  }
+}
+```
+
+## TODO
+
+- Delete a comment
+- Authentication
---
+++ src/index.js
@@ -0,0 +1,108 @@
+// Set up Firebase
+import * as firebase from "firebase";
+var config = require('../config.json');
+
+firebase.initializeApp(config);
+
+// create helper functions
+function writeComment(commentData) {
+    // Get a key for a new Comment.
+    var newCommentsKey = firebase.database().ref().child('comments').push().key;
+
+    // Write the new comments's data simultaneously in the comments list and the user's comments list.
+    var updates = {};
+    updates['/comments/' + commentData.componentId + '/' + commentData.version + '/'  + newCommentsKey] = commentData;
+    // updates['/user-comments/' + commentData.userName + '/' + commentData.componentId + '/' + commentData.version + '/' + newCommentsKey] = commentData;
+
+    try {
+        firebase.database().ref().update(updates);
+        return JSON.parse('{ "success": 1 }');
+    } catch (error) {
+        var errorMsg = error ? JSON.stringify(error) : 'There was a problem writing the data to the database.';
+        return JSON.parse('{ "success": 0, "error": ' + errorMsg + '}');
+    }
+
+}
+
+function readComments (componentId) {
+    try {
+        return firebase.database().ref('/comments/' + componentId).once('value').then(function(snapshot) {
+            var allComments = snapshot.val();
+
+            if (allComments === null) {
+                return JSON.parse('{ "success": 0, "error":  "No comments found for this component." }');
+            }
+            return JSON.parse('{ "success": 1, "comments": ' + JSON.stringify(allComments) + ' }');
+        });
+    } catch (error) {
+        console.log(error);
+        var errorMsg = error ? JSON.stringify(error) : 'There was a problem reading the data from the database.';
+        return JSON.parse('{ "success": 0, "error": ' + errorMsg + '}');
+    }
+}
+
+
+// create microservice
+import url from 'url';
+
+import micro, {
+    json,
+    send,
+    sendError,
+} from 'micro';
+
+/**
+ * handle POST requests
+ */
+async function postHandler(request) {
+    const postParams = await json(request);
+    const { pathname } = url.parse(request.url);
+
+    var commentData = {
+        userName: postParams.userName,
+        userEmail: postParams.userEmail,
+        componentId: pathname,
+        comment: postParams.comment,
+        timestamp: new Date().getTime()
+    };
+
+    return await writeComment(commentData);
+}
+
+/**
+ * handle GET requests
+ */
+async function getHandler(request) {
+    const { pathname } = url.parse(request.url);
+    return await readComments(pathname);
+}
+
+/**
+ * Check the request method and use postHandler or getHandler (or other method handlers)
+ */
+async function methodHandler(request, response) {
+    try {
+        switch (request.method) {
+            case 'POST':
+                return await postHandler(request);
+            case 'GET':
+                return await getHandler(request);
+            default:
+                send(response, 405, 'Invalid method');
+                break;
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+/**
+ * our micro service, run methodHandler and send result as a response (or an error)
+ */
+module.exports = async function (request, response) {
+    try {
+        send(response, 200, await methodHandler(request));
+    } catch (error) {
+        sendError(request, response, error);
+    }
+};
